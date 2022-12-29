f9fc49ab0a572c1212efafaf178fa86710176381
lauw14 <lauw14@hotmail.com>
2022-12-29 20:31:13 +0800

    add header

--- products.py
+++ products.py
@@ -13,3 +13,10 @@ print(products)
 
 for product in products:
 	print(product[0], '的價格是', product[1])
+
+with open('products.csv', 'w', encoding='utf-8') as f: #w是寫入模式，會產生products.txt
+	#也會覆蓋原來的products檔
+	#f是代表這個txt檔案
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
